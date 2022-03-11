ec95686231b70d7a386bb0acfe7bb41883f8efed
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2022-03-11 23:51:58 +0100

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -6,7 +6,7 @@ const Schema = require('../../models/custom-commands');
 const config = require("../../botconfig/config.json"); //loading config file with token and prefix, and settings
 const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
 const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
-const { escapeRegex} = require("../../functions"); //Loading all needed functions
+const { escapeRegex} = require("../../handlers/functions"); //Loading all needed functions
 //here the event starts
 module.exports = async (client, message) => {
   try {
