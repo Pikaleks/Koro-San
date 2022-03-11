c14eebd8aea33b2c0327503ee77f0fa113845bf8
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2022-03-11 23:16:06 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -10,6 +10,31 @@ module.exports = client => {
     count += guild.memberCount // <---
 
 })
+    setInterval(async function(){
+    const fetch = require("node-superfetch");
+    let user = ["moebeams", "ryval_alex"]
+    for (var i = 0; i < 2; i++) {
+      const uptime = await fetch.get(`http://decapi.me/twitch/uptime/${user[i]}`)
+      const title = await fetch.get(`http://decapi.me/twitch/title/${user[i]}`)
+  
+      const twitch = require("../../handlers/models/twitch-schema")
+      let data = await twitch.findOne({ user: user[i] }, { titletwitch: title.body })
+      if(`${uptime.body}` === `${user[i]} is offline`) {
+        await twitch.findOneAndDelete({ user: user[i] })
+      } else {
+        if (!data)  {
+          data = new twitch({
+            user: user[i],
+            titletwitch: `${title.body}`
+          })
+
+          await client.channels.cache.get("837027849931194432").send({ content: `<@&951964510867046410>\n${user[i]} est en LIVE BANGER!\nhttps://www.twitch.tv/${user[i]}`})
+
+          data.save()
+        }
+      }
+    }
+  }, 300000)
   try{
     client.user.setActivity(`${count} users`, { type: "WATCHING" });
   }catch (e) {
